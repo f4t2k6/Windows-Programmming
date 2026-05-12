eed9e53bda2c6fede66781a8677d8c3c54d34a21
f4t2k6 <huyphat06112006@gmail.com>
2026-05-12 09:18:59 +0700

    fix upload

--- 2-Introduction_Forms/f_AddStudent.Designer.cs
+++ 2-Introduction_Forms/f_AddStudent.Designer.cs
@@ -43,109 +43,68 @@
             btnClear = new Button();
             ((System.ComponentModel.ISupportInitialize)picStudent).BeginInit();
             SuspendLayout();
-            // 
+
             // txtMSSV
-            // 
             txtMSSV.Location = new Point(88, 192);
             txtMSSV.Name = "txtMSSV";
             txtMSSV.Size = new Size(178, 27);
             txtMSSV.TabIndex = 1;
             txtMSSV.Text = "Nhập MSSV";
-<<<<<<< Updated upstream
-=======
-            txtMSSV.Click += txtMSSV_Click;
->>>>>>> Stashed changes
             txtMSSV.KeyPress += txtMSSV_KeyPress;
-            // 
+
             // txtFname
-            // 
             txtFname.Location = new Point(88, 239);
             txtFname.Name = "txtFname";
             txtFname.Size = new Size(178, 27);
             txtFname.TabIndex = 3;
             txtFname.Text = "Nhập họ và tên đệm";
-<<<<<<< Updated upstream
-            txtFname.TextChanged += textBox2_TextChanged;
-=======
-            txtFname.Click += txtFname_Click;
->>>>>>> Stashed changes
             txtFname.KeyPress += txtFname_KeyPress;
-            // 
+
             // txtLname
-            // 
-<<<<<<< Updated upstream
             txtLname.Location = new Point(88, 286);
-=======
-            txtLname.Location = new Point(88, 285);
->>>>>>> Stashed changes
             txtLname.Name = "txtLname";
             txtLname.Size = new Size(178, 27);
             txtLname.TabIndex = 5;
             txtLname.Text = "Nhập tên";
-<<<<<<< Updated upstream
             txtLname.KeyPress += txtLname_KeyPress;
-=======
-            txtLname.Click += txtLname_Click;
-            txtLname.KeyPress += txtFname_KeyPress;
->>>>>>> Stashed changes
-            // 
+
             // txtHometown
-            // 
             txtHometown.Location = new Point(461, 239);
             txtHometown.Name = "txtHometown";
             txtHometown.Size = new Size(178, 27);
             txtHometown.TabIndex = 13;
             txtHometown.Text = "Nhập quê quán";
-            // 
+
             // txtAddress
-            // 
             txtAddress.Location = new Point(461, 192);
             txtAddress.Name = "txtAddress";
             txtAddress.Size = new Size(178, 27);
             txtAddress.TabIndex = 11;
             txtAddress.Text = "Nhập địa chỉ";
-            // 
+
             // txtPhone
-            // 
             txtPhone.Location = new Point(461, 144);
             txtPhone.Name = "txtPhone";
             txtPhone.Size = new Size(178, 27);
             txtPhone.TabIndex = 9;
             txtPhone.Text = "Nhập số điện thoại";
-<<<<<<< Updated upstream
-=======
-            txtPhone.Click += txtPhone_Click;
->>>>>>> Stashed changes
             txtPhone.KeyPress += txtMSSV_KeyPress;
-            // 
+
             // picStudent
-            // 
             picStudent.AccessibleName = "picStudent";
-<<<<<<< Updated upstream
             picStudent.BackColor = SystemColors.HighlightText;
             picStudent.BackgroundImageLayout = ImageLayout.Center;
             picStudent.BorderStyle = BorderStyle.FixedSingle;
             picStudent.Location = new Point(126, 33);
             picStudent.Name = "picStudent";
             picStudent.Size = new Size(90, 90);
-=======
-            picStudent.BorderStyle = BorderStyle.FixedSingle;
-            picStudent.Location = new Point(126, 33);
-            picStudent.Name = "picStudent";
-            picStudent.Size = new Size(90, 91);
->>>>>>> Stashed changes
             picStudent.SizeMode = PictureBoxSizeMode.Zoom;
             picStudent.TabIndex = 18;
             picStudent.TabStop = false;
-            // 
+
             // dtpDob
-            // 
-<<<<<<< Updated upstream
             dtpDob.CustomFormat = "dd/MM/yyyy";
             dtpDob.Format = DateTimePickerFormat.Custom;
-=======
-            dtpDob.Format = DateTimePickerFormat.Short;
->>>>>>> Stashed changes
             dtpDob.Location = new Point(461, 48);
             dtpDob.MaxDate = new DateTime(2008, 12, 31, 0, 0, 0, 0);
             dtpDob.MinDate = new DateTime(1900, 1, 1, 0, 0, 0, 0);
@@ -154,39 +113,25 @@
             dtpDob.TabIndex = 20;
             dtpDob.Value = new DateTime(2008, 1, 1, 0, 0, 0, 0);
             dtpDob.ValueChanged += dtpDob_ValueChanged;
-            // 
+
             // txtEmail
-            // 
-<<<<<<< Updated upstream
             txtEmail.Location = new Point(461, 286);
-=======
-            txtEmail.Location = new Point(461, 285);
->>>>>>> Stashed changes
             txtEmail.Name = "txtEmail";
             txtEmail.Size = new Size(178, 27);
             txtEmail.TabIndex = 21;
             txtEmail.Text = "Nhập email";
-            // 
+
             // cboGender
-            // 
             cboGender.FormattingEnabled = true;
-<<<<<<< Updated upstream
             cboGender.Items.AddRange(new object[] { "Nam", "Nữ", "Khác" });
-=======
->>>>>>> Stashed changes
             cboGender.Location = new Point(461, 95);
             cboGender.Name = "cboGender";
             cboGender.Size = new Size(178, 28);
             cboGender.TabIndex = 22;
-            cboGender.Text = "Items: Nam, Nữ, Khác";
             cboGender.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
-            // 
+
             // btnChooseImage
-            // 
-<<<<<<< Updated upstream
             btnChooseImage.BackColor = SystemColors.ScrollBar;
-=======
->>>>>>> Stashed changes
             btnChooseImage.Location = new Point(88, 144);
             btnChooseImage.Name = "btnChooseImage";
             btnChooseImage.Size = new Size(178, 29);
@@ -194,49 +139,31 @@
             btnChooseImage.Text = "Chọn ảnh từ máy tính";
             btnChooseImage.UseVisualStyleBackColor = false;
             btnChooseImage.Click += btnChooseImage_Click;
-            // 
+
             // btnAdd
-            // 
-<<<<<<< Updated upstream
             btnAdd.BackColor = SystemColors.ScrollBar;
             btnAdd.Location = new Point(427, 346);
-=======
-            btnAdd.BackColor = Color.Transparent;
-            btnAdd.Location = new Point(427, 347);
->>>>>>> Stashed changes
             btnAdd.Name = "btnAdd";
             btnAdd.Size = new Size(101, 72);
             btnAdd.TabIndex = 24;
             btnAdd.Text = "Thêm sinh viên";
             btnAdd.UseVisualStyleBackColor = false;
             btnAdd.Click += btnAdd_Click;
-            // 
+
             // btnClear
-            // 
-<<<<<<< Updated upstream
             btnClear.BackColor = SystemColors.ControlLight;
             btnClear.Location = new Point(574, 346);
-=======
-            btnClear.Location = new Point(574, 347);
->>>>>>> Stashed changes
             btnClear.Name = "btnClear";
             btnClear.Size = new Size(101, 72);
             btnClear.TabIndex = 25;
             btnClear.Text = "Xóa trắng form";
             btnClear.UseVisualStyleBackColor = false;
-            // 
+
             // f_AddStudent
-            // 
-            AccessibleDescription = "";
-            AccessibleName = "";
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaption;
-<<<<<<< Updated upstream
             ClientSize = new Size(800, 450);
-=======
-            ClientSize = new Size(743, 427);
->>>>>>> Stashed changes
             Controls.Add(btnClear);
             Controls.Add(btnAdd);
             Controls.Add(btnChooseImage);
@@ -259,6 +186,7 @@
         }
 
         #endregion
+
         private TextBox txtMSSV;
         private TextBox txtFname;
         private TextBox txtLname;
--- 2-Introduction_Forms/f_AddStudent.cs
+++ 2-Introduction_Forms/f_AddStudent.cs
@@ -17,37 +17,17 @@ namespace Day01
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
+        private void label1_Click(object sender, EventArgs e) { }
+        private void label2_Click(object sender, EventArgs e) { }
+        private void label10_Click(object sender, EventArgs e) { }
 
-        }
+        private void f_AddStudent_Load(object sender, EventArgs e) { }
 
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label10_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void f_AddStudent_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
+        private void textBox2_TextChanged(object sender, EventArgs e) { }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) { }
 
         byte[]? studentImage = null;
+
         private void btnChooseImage_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
@@ -55,40 +35,31 @@ namespace Day01
             if (ofd.ShowDialog() == DialogResult.OK)
             {
                 picStudent.Image = Image.FromFile(ofd.FileName);
-                // Chuyển ảnh sang byte array
                 MemoryStream ms = new MemoryStream();
                 picStudent.Image.Save(ms, picStudent.Image.RawFormat);
                 studentImage = ms.ToArray();
             }
         }
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            // 1. Kiểm tra các ô chữ (Code cũ đã có)
             if (string.IsNullOrEmpty(txtMSSV.Text) || string.IsNullOrEmpty(txtLname.Text))
             {
                 MessageBox.Show("Vui lòng nhập đầy đủ MSSV và Tên!", "Cảnh báo");
                 return;
             }
 
-            // 2. KIỂM TRA ẢNH: Bắt buộc phải chọn ảnh
             if (studentImage == null)
             {
                 MessageBox.Show("Vui lòng chọn ảnh đại diện cho sinh viên!", "Cảnh báo");
                 return;
             }
 
-            // 3. Khởi tạo Student
-            // Lúc này truyền studentImage vào sẽ KHÔNG CÒN BỊ CẢNH BÁO VÀNG NỮA
             Student sv = new Student(
                 int.Parse(txtMSSV.Text), txtFname.Text, txtLname.Text,
                 dtpDob.Value, cboGender.Text, txtPhone.Text,
                 txtAddress.Text, txtHometown.Text, txtEmail.Text, studentImage);
 
-            // 4. Lưu vào database
             if (sv.AddStudent())
                 MessageBox.Show("Thêm sinh viên thành công!", "Thông báo");
             else
@@ -98,54 +69,21 @@ namespace Day01
         private void txtMSSV_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
-            {
-                // Thì hủy bỏ, không cho phím đó in ra màn hình
                 e.Handled = true;
-            }
         }
 
         private void txtLname_KeyPress(object sender, KeyPressEventArgs e)
         {
-            // Cho phép phím điều khiển (Backspace), chữ cái, và dấu cách (khoảng trắng)
             if (!char.IsControl(e.KeyChar) && !char.IsLetter(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
-            {
-                // Chặn các phím còn lại (số, ký tự đặc biệt...)
                 e.Handled = true;
-            }
         }
 
         private void txtFname_KeyPress(object sender, KeyPressEventArgs e)
         {
-            // Cho phép phím điều khiển (Backspace), chữ cái, và dấu cách (khoảng trắng)
             if (!char.IsControl(e.KeyChar) && !char.IsLetter(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
-            {
-                // Chặn các phím còn lại (số, ký tự đặc biệt...)
                 e.Handled = true;
-            }
         }
 
-        private void dtpDob_ValueChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtMSSV_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
-            {
-                // Thì hủy bỏ, không cho phím đó in ra màn hình
-                e.Handled = true;
-            }
-        }
-
-        private void txtFname_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            // Cho phép phím điều khiển (Backspace), chữ cái, và dấu cách (khoảng trắng)
-            if (!char.IsControl(e.KeyChar) && !char.IsLetter(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
-            {
-                // Chặn các phím còn lại (số, ký tự đặc biệt...)
-                e.Handled = true;
-            }
-        }
+        private void dtpDob_ValueChanged(object sender, EventArgs e) { }
     }
 }
